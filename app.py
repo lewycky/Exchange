a327acc3d21329b2040fa57ca57aa4a9f70b6eac
Arek Lewicki <lewicki1arek@gmail.com>
2022-09-16 19:41:58 +0200

    Upgrade this files

--- app.py
+++ app.py
@@ -1,52 +1,41 @@
-from urllib import response
-from flask import render_template
+
+from locale import currency
 import requests
-import json, pickle
 import csv
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
-response = requests.get("https://api.nbp.pl/api/exchangerates/tables/C?format=json")
-
+response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
-data_as_jason=json.dumps(data)
-
-
-rates=[]
-
-for item in data:
-    rates.append(item['rates'])
-
-
-with open('file.csv', 'w') as f:
-    for row in rates:
-        for x in row:
-            f.write(str(x)+ ';')
-            f.write('\n')
-
-
-code=[   
-{'currency': 'dolar amerykański', 'code': 'USD', 'bid': 4.6738, 'ask': 4.7682},
-{'currency': 'dolar australijski', 'code': 'AUD', 'bid': 3.1489, 'ask': 3.2125},
-{'currency': 'dolar kanadyjski', 'code': 'CAD', 'bid': 3.5488, 'ask': 3.6204},
-{'currency': 'euro', 'code': 'EUR', 'bid': 4.67, 'ask': 4.7644},
-{'currency': 'forint węgierski', 'code': 'HUF', 'bid': 0.011528, 'ask': 0.01176},
-{'currency': 'frank szwajcarski', 'code': 'CHF', 'bid': 4.8598, 'ask': 4.958},
-{'currency': 'funt szterling', 'code': 'GBP', 'bid': 5.407, 'ask': 5.5162},
-{'currency': 'jen japoński', 'code': 'JPY', 'bid': 0.032708, 'ask': 0.033368},
-{'currency': 'korona czeska', 'code': 'CZK', 'bid': 0.1905, 'ask': 0.1943},
-{'currency': 'korona duńska', 'code': 'DKK', 'bid': 0.628, 'ask': 0.6406},
-{'currency': 'korona norweska', 'code': 'NOK', 'bid': 0.4632, 'ask': 0.4726},
-{'currency': 'korona szwedzka', 'code': 'SEK', 'bid': 0.4379, 'ask': 0.4467},
-{'currency': 'SDR (MFW)', 'code': 'XDR', 'bid': 5.9977, 'ask': 6.1189}
-]
-
-
+rates = data[0].get('rates')
 
 app = Flask(__name__)
 
-@app.route('/')
+def get_codes():
+    codes = []
+    for data in rates:
+        codes.append(data.get('code'))
+    return sorted(codes)
+
+codes = get_codes()
+
+with open('rates.csv', 'w') as csvfile:
+    fieldnames = ['currency', 'code', 'bid', 'ask']
+    writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(rates)
+
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    return render_template('index.html')
+    if request.method == "POST":
+        data = request.form
+        code = data.get('code')
+        amount = data.get('amount')
+        ask = data.get('ask')
+        for rate in rates:
+            if rate['code'] == request.form['codes']:
+                wynik= round((float((rate['ask'])) * float(amount)), 2)
+    return render_template("index.html", codes=codes, data=data, result=wynik )
 
-   
 
+if __name__ == "__main__":
+    app.run(debug=True)   
